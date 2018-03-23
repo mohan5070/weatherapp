ee02a46a9633bb48d1b9920170d75dec787c7a85
mohan <mohan5070@gmail.com>
2018-03-23 08:37:07 +0530

    build folder created

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,7 +3,6 @@ import weatherApp from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
-    console.log('initialState: ' + initialState);
     return createStore(
         weatherApp,
         initialState,
